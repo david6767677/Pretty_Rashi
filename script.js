d92639bf0f3a01a2c7e20f48bd7b57cb12046364
yoyo <157238943+david6767677@users.noreply.github.com>
2024-05-27 01:28:50 +0530

    script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function startQuestions() {
       document.getElementById("init").innerText = "YAY YAY 🎉🎉🎉";
     document.getElementById("message").style.display = "block";
     document.getElementById("messageText").innerText = "I believe in Love and Destiny too! How about we go out for a Coffee ☕ and Banoffee 🥧 to discuss if it's true?";
-    document.getElementById("message").style.display = "block";
     document.getElementById("messageText").innerText ="(p.s. i also know places with amazing Cranberry 🍒 cocktails)";
       showConfetti()
 }
